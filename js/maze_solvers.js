88fdc3f0773c8a3230f62757c00776791c02df4d
shuvo8904654 <salhasan627@gmail.com>
2026-03-03 10:15:00 +0600

    Add Dijkstra, greedy best-first and A* search algorithms

--- js/maze_solvers.js
+++ js/maze_solvers.js
@@ -1,10 +1,7 @@
 "use strict";
 
 function distance(point_1, point_2) {
-    return Math.sqrt(
-        Math.pow(point_2[0] - point_1[0], 2) +
-        Math.pow(point_2[1] - point_1[1], 2)
-    );
+    return Math.sqrt(Math.pow(point_2[0] - point_1[0], 2) + Math.pow(point_2[1] - point_1[1], 2));
 }
 
 function get_neighbours(cell, distance) {
@@ -49,24 +46,90 @@ function maze_solvers_interval() {
     my_interval = requestAnimationFrame(animate);
 }
 
-function breadth_first() {
-    var frontier = [start_pos];
-    var head = 0;
-    grid[start_pos[0]][start_pos[1]] = 1;
-    node_list = []; found = false;
-    while (head < frontier.length && !found) {
-        var curr = frontier[head++];
-        var list = get_neighbours(curr, 1);
-        for (var i = 0; i < list.length; i++) {
-            if (get_node(list[i][0], list[i][1]) === 0) {
-                frontier.push(list[i]);
-                grid[list[i][0]][list[i][1]] = i + 1;
-                if (list[i][0] === target_pos[0] && list[i][1] === target_pos[1]) { found = true; break; }
-                node_list.push(list[i]);
+function run_solver(type, silent) {
+    var results = { nodes: [], path: [], found: false, cost: 0, time: 0 };
+    var original_grid = silent ? JSON.parse(JSON.stringify(grid)) : null;
+    var startTime = Date.now();
+    var frontier;
+    var cost_grid = new Array(grid.length).fill(0).map(() => new Array(grid[0].length).fill(Infinity));
+
+    if (type === "1") {
+        frontier = [start_pos]; var head = 0;
+        grid[start_pos[0]][start_pos[1]] = 1;
+        while (head < frontier.length && !results.found) {
+            var curr = frontier[head++];
+            var list = get_neighbours(curr, 1);
+            for (var i = 0; i < list.length; i++) {
+                if (get_node(list[i][0], list[i][1]) === 0) {
+                    frontier.push(list[i]);
+                    grid[list[i][0]][list[i][1]] = i + 1;
+                    if (list[i][0] === target_pos[0] && list[i][1] === target_pos[1]) { results.found = true; break; }
+                    results.nodes.push(list[i]);
+                }
+            }
+        }
+    } else if (type === "3" || type === "4" || type === "5") {
+        var compareFn;
+        if (type === "3") compareFn = (a, b) => distance(a, target_pos) - distance(b, target_pos);
+        else if (type === "4") compareFn = (a, b) => cost_grid[a[0]][a[1]] - cost_grid[b[0]][b[1]];
+        else compareFn = (a, b) => (cost_grid[a[0]][a[1]] + distance(a, target_pos)) - (cost_grid[b[0]][b[1]] + distance(b, target_pos));
+
+        frontier = new MinHeap(compareFn);
+        frontier.push(start_pos);
+        cost_grid[start_pos[0]][start_pos[1]] = 0;
+        grid[start_pos[0]][start_pos[1]] = 1;
+        while (frontier.size() > 0 && !results.found) {
+            var current = frontier.pop();
+            var list = get_neighbours(current, 1);
+            for (var i = 0; i < list.length; i++) {
+                if (get_node(list[i][0], list[i][1]) === 0) {
+                    var w = weights[list[i][0]][list[i][1]];
+                    var new_cost = cost_grid[current[0]][current[1]] + w;
+                    if (new_cost < cost_grid[list[i][0]][list[i][1]]) {
+                        cost_grid[list[i][0]][list[i][1]] = new_cost;
+                        frontier.push(list[i]);
+                        grid[list[i][0]][list[i][1]] = i + 1;
+                        if (list[i][0] === target_pos[0] && list[i][1] === target_pos[1]) { results.found = true; break; }
+                        results.nodes.push(list[i]);
+                    }
+                }
             }
         }
     }
-    path_list = found ? reconstruct_path(target_pos, start_pos) : [];
+
+    if (results.found) {
+        results.path = reconstruct_path(target_pos, start_pos);
+        results.cost = results.path.reduce((acc, curr) => acc + weights[curr[0]][curr[1]], 0) + weights[target_pos[0]][target_pos[1]];
+    }
+    results.time = Date.now() - startTime;
+    if (silent) { grid = original_grid; }
+    return results;
+}
+
+function breadth_first() {
+    var res = run_solver("1", false);
+    node_list = res.nodes; path_list = res.path; found = res.found; total_cost = res.cost;
+    node_list_index = 0; path_list_index = 0; path = false;
+    maze_solvers_interval();
+}
+
+function greedy_best_first() {
+    var res = run_solver("3", false);
+    node_list = res.nodes; path_list = res.path; found = res.found; total_cost = res.cost;
+    node_list_index = 0; path_list_index = 0; path = false;
+    maze_solvers_interval();
+}
+
+function dijkstra() {
+    var res = run_solver("4", false);
+    node_list = res.nodes; path_list = res.path; found = res.found; total_cost = res.cost;
+    node_list_index = 0; path_list_index = 0; path = false;
+    maze_solvers_interval();
+}
+
+function a_star() {
+    var res = run_solver("5", false);
+    node_list = res.nodes; path_list = res.path; found = res.found; total_cost = res.cost;
     node_list_index = 0; path_list_index = 0; path = false;
     maze_solvers_interval();
 }
@@ -83,5 +146,8 @@ function maze_solvers() {
     var solver_value = document.getElementById("slct_1").value;
     switch (solver_value) {
         case "1": breadth_first(); break;
+        case "3": greedy_best_first(); break;
+        case "4": dijkstra(); break;
+        case "5": a_star(); break;
     }
 }
--- js/properties.js
+++ js/properties.js
@@ -23,16 +23,65 @@ var path_list;
 var path_list_index;
 var found = false;
 var path = false;
+var total_cost = 0;
 
 var generating = false;
 var timeouts = [];
 var visualization_speed = 50;
 var is_mobile = false;
 
+function MinHeap(compareFn) {
+    this.data = [];
+    this.compare = compareFn;
+}
+
+MinHeap.prototype.size = function () { return this.data.length; };
+
+MinHeap.prototype.push = function (val) {
+    this.data.push(val);
+    this._bubbleUp(this.data.length - 1);
+};
+
+MinHeap.prototype.pop = function () {
+    var top = this.data[0];
+    var bottom = this.data.pop();
+    if (this.data.length > 0) { this.data[0] = bottom; this._sinkDown(0); }
+    return top;
+};
+
+MinHeap.prototype.peek = function () { return this.data[0]; };
+
+MinHeap.prototype._bubbleUp = function (idx) {
+    var element = this.data[idx];
+    while (idx > 0) {
+        var parentIdx = Math.floor((idx - 1) / 2);
+        var parent = this.data[parentIdx];
+        if (this.compare(element, parent) >= 0) break;
+        this.data[idx] = parent;
+        idx = parentIdx;
+    }
+    this.data[idx] = element;
+};
+
+MinHeap.prototype._sinkDown = function (idx) {
+    var length = this.data.length;
+    var element = this.data[idx];
+    while (true) {
+        var leftIdx = 2 * idx + 1;
+        var rightIdx = 2 * idx + 2;
+        var smallest = idx;
+        if (leftIdx < length && this.compare(this.data[leftIdx], this.data[smallest]) < 0) smallest = leftIdx;
+        if (rightIdx < length && this.compare(this.data[rightIdx], this.data[smallest]) < 0) smallest = rightIdx;
+        if (smallest === idx) break;
+        this.data[idx] = this.data[smallest];
+        this.data[smallest] = element;
+        idx = smallest;
+    }
+};
+
 function reconstruct_path(target, start) {
     var path_list = [];
     var current_node = target;
-
     while (current_node[0] !== start[0] || current_node[1] !== start[1]) {
         switch (grid[current_node[0]][current_node[1]]) {
             case 1: current_node = [current_node[0], current_node[1] + 1]; break;
@@ -43,7 +92,6 @@ function reconstruct_path(target, start) {
         }
         path_list.push(current_node);
     }
-
     path_list.pop();
     path_list.reverse();
     return path_list;
